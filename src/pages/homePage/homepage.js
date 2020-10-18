4233090b74c68198d6f723df751d9e094ae8df5e
Ciubotaru Rares <rares.ciubotaru01@gmail.com>
2020-10-18 15:19:52 +0300

    broken homepage into components + styling the menu

---
+++ src/pages/homePage/homepage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./homepage.scss";
+
+import Directory from "../../components/directory/directory";
+
+const homePage = () => {
+  return (
+    <div className="homePage">
+      <Directory />
+    </div>
+  );
+};
+
+export default homePage;
