e65c1836e4256d3aec168df862954c7e3d6b199a
Ciubotaru Rares <rares.ciubotaru01@gmail.com>
2020-10-26 14:45:19 +0200

    adding header and links

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import "./App.css";
 import HomePage from "./pages/homePage/homepage";
 import { Route } from "react-router-dom";
-import ShopPAge from "./pages/shop/shop";
+import ShopPage from "./pages/shop/shop";
+import Header from "./components/header/header";
 
 function App() {
   return (
     <div>
+      <Header />
       <Route exact path="/" component={HomePage} />
-      <Route path="/shop" component={ShopPAge} />
+      <Route path="/shop" component={ShopPage} />
     </div>
   );
 }
